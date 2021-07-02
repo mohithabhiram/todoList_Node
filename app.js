037f742dbb95432ccb5f3cf50289340899de9a0e
Mohith Abhiram <you@example.com>
2021-07-02 14:15:59 +0530

    Connected to mongoose and rendered default items to the todolist app by adding a todolistDB

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
+const mongoose = require("mongoose");
 const app = express();
-let newItems = [];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true, useUnifiedTopology: true});
+//Schema Creation
+const newItemsSchema = {
+  name: String
+};
+//Model Creation
+const Item = mongoose.model("Item",newItemsSchema);
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1,item2,item3];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.get("/", function(req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day=today.toLocaleDateString("en-US",options);
-
-  res.render("list", {
-    listTitle: day,newListItems:newItems
+  Item.find({},function(err,foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems,function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully saved default items to database");
+        }
+      });
+      res.redirect("/");
+    }
+    else{
+      res.render("list", {
+        listTitle:"Today",newListItems: foundItems
+      });
+    }
   });
 });
 app.post("/",function(req,res){
